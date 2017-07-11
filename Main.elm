c5317f6404920c03e83cc191eef73612bea64a8d
isberg <jonas.d.isberg@gmail.com>
2017-07-11 18:04:22 +0200

    feat: basic backlog with add, remove, edit

--- Main.elm
+++ Main.elm
@@ -1,4 +1,37 @@
-import Html exposing (text)
+import Html exposing (beginnerProgram, div, button, text, h1, ul, li, input)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (value)
+
 
 main =
-  text "Hello, World!"
+  beginnerProgram { model = model, view = view, update = update }
+
+
+model = { backlog = [ "Story 1", "Story 2"], new = "" }
+
+view model =
+  div []
+    [ h1 [] [ text "Backlog" ]
+    , storyListView model.backlog
+    , input [onInput Change, value model.new] []
+    , button [onClick Add ] [text "Add"]
+    ]
+
+storyListView model = 
+  let
+    itemView item = li[onClick (Remove item)] [text item]
+  in
+    ul [] (List.map itemView model)
+  
+
+type Msg = Add | Change String | Remove String
+
+
+update msg model = 
+  case msg of
+    Add ->
+      {model | backlog = model.new :: model.backlog, new = "" }
+    Change new ->
+      {model | new = new }
+    Remove story ->
+      {model | backlog = (List.filter (\x -> x /= story) model.backlog), new = story}
