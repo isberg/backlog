677e7a2858cb034b8d34a3c7e4794dd66fb13c91
isberg <jonas.d.isberg@gmail.com>
2017-07-21 10:57:17 +0200

    feat: (auto-)focus on input

--- Main.elm
+++ Main.elm
@@ -1,10 +1,20 @@
-import Html exposing (beginnerProgram, div, button, text, h1, ul, li, input)
+import Html exposing (program, div, button, text, h1, ul, li, input)
 import Html.Events exposing (onClick, onInput)
-import Html.Attributes exposing (value)
+import Html.Attributes exposing (value, autofocus, id)
+import Dom
+import Task
 
 
 main =
-  beginnerProgram { model = model, view = view, update = update }
+  program { init = init, view = view, update = update , subscriptions = (\_ -> Sub.none)}
+
+init = 
+  ( { backlog = [ "Story 1", "Story 2"]
+    , new = "" 
+    }
+  , Cmd.none
+  )
+
 
 type alias Model = { backlog : List String, new : String }
 model = 
@@ -16,8 +26,8 @@ view model =
   div []
     [ h1 [] [ text "Backlog" ]
     , storiesView model.backlog
-    , input [onInput Change, value model.new] []
-    , button [onClick Add ] [text "Add"]
+    , input [onInput Change, value model.new, autofocus True, id "newStory"] []
+    , button [onClick Add] [text "Add"]
     ]
 
 storiesView model = 
@@ -31,18 +41,34 @@ type Msg
   = Add 
   | Change String 
   | Remove String
+  | FocusResult (Result Dom.Error ())
 
 
 update msg { backlog, new } = 
   case msg of
     Add ->
       Model (new :: backlog) "" 
+      ! [ Dom.focus "newStory" |> Task.attempt FocusResult ]
 
     Change newer ->
       Model backlog newer 
+      ! [ Dom.focus "newStory" |> Task.attempt FocusResult ]
 
     Remove story ->
       let
         backlog_ = List.filter (\x -> x /= story) backlog
       in
         Model backlog_ story 
+        ! [ Dom.focus "newStory" |> Task.attempt FocusResult ]
+
+    FocusResult result ->
+      -- handle success or failure here
+      case result of
+        Err (Dom.NotFound id) ->
+          -- unable to find dom 'id'
+          Model backlog new
+          ! [ Cmd.none ]
+        Ok () ->
+          -- successfully focus the dom
+          Model backlog new
+          ! [ Cmd.none ]
